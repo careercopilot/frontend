f1510d616c8cdcf3550984fe609244af8ed3a569
Raj <varasaniraj5@gmail.com>
2023-05-08 16:03:02 +0530

    update header for logged in preview

--- src/components/general/Header.tsx
+++ src/components/general/Header.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "@mantine/core";
+import { Button, Avatar } from "@mantine/core";
 import { staticData } from "@/utils/staticData";
 
 import styles from "./Header.module.css";
+import User from "@/interfaces/User";
 
 const { navbar: COMPONENT_DATA } = staticData.components;
 const generalData = staticData.general;
 
 function Header() {
+  /** TODO: Fetched Data */
+  const userData: User | null = {
+    _id: "random",
+    fName: "Raj",
+    lName: "Varsani",
+    email: "zairestanton@gmail.com",
+  };
+
   return (
     <nav className={styles.container}>
       <Link href="/">
@@ -25,36 +34,50 @@ function Header() {
           className={styles.logoIcon}
         />
       </Link>
-      <ul className={styles.buttons}>
-        <li>
-          <Link
-            href={{
-              query: {
-                modal: COMPONENT_DATA.login.path,
-              },
-            }}
-            tabIndex={-1}
-          >
-            <Button color="secondary" variant="subtle" size="md">
-              {COMPONENT_DATA.login.name}
-            </Button>
-          </Link>
-        </li>
-        <li>
-          <Link
-            href={{
-              query: {
-                modal: COMPONENT_DATA.register.path,
-              },
-            }}
-            tabIndex={-1}
-          >
-            <Button color="secondary" size="md">
-              {COMPONENT_DATA.register.name}
-            </Button>
-          </Link>
-        </li>
-      </ul>
+      {userData ? (
+        <Avatar
+          src={userData.image}
+          alt={userData.fName + " " + userData.lName}
+          color="secondary"
+          component={Link}
+          href={"/profile"}
+
+
+        >
+          {userData.image ? null : userData.fName[0] + userData.lName[0]}
+        </Avatar>
+      ) : (
+        <ul className={styles.buttons}>
+          <li>
+            <Link
+              href={{
+                query: {
+                  modal: COMPONENT_DATA.login.path,
+                },
+              }}
+              tabIndex={-1}
+            >
+              <Button color="secondary" variant="subtle" size="md">
+                {COMPONENT_DATA.login.name}
+              </Button>
+            </Link>
+          </li>
+          <li>
+            <Link
+              href={{
+                query: {
+                  modal: COMPONENT_DATA.register.path,
+                },
+              }}
+              tabIndex={-1}
+            >
+              <Button color="secondary" size="md">
+                {COMPONENT_DATA.register.name}
+              </Button>
+            </Link>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 }
---
+++ src/interfaces/User.ts
@@ -0,0 +1,10 @@
+
+interface User {
+  _id: string;
+  fName: string;
+  lName: string;
+  image?: string;
+  email: string;
+}
+
+export default User;
